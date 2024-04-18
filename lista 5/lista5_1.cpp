02a26ea1ac95a8ed28d05413b4f3d7190a87b65c
dudavidal <164089944+dudavidal@users.noreply.github.com>
2024-04-18 14:35:47 -0300

    Update lista5_1.cpp

--- lista 5/lista5_1.cpp
+++ lista 5/lista5_1.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+void insertionSort (string nomes[], int tamanho)
+{
+    for (int j = 1; j < tamanho; ++j) //j=1 para permitir a comparação do segundo nome com o primeiro.
+    {
+        string chave = nomes[j];
+        int i = j - 1; // parcela ordenada
+        
+        while (i >= 0 && nomes[i] > chave) // continua se tiver elementos a esquerda e é maior que a chave
+        {
+            
+            nomes[i + 1] = nomes[i]; // joga o i pra um na frente
+            i = i - 1; //atualiza para apontar para o próximo elemento à esquerda
+        }
+        
+        nomes[i + 1] = chave; // troca de lugar 
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Quantos nomes você deseja inserir? ";
+    cin >> n;
+
+    string nomes[n]; 
+    
+    for (int i = 0; i < n; ++i) // le todos os nomes
+    {
+        cout << "Insira o nome " << i + 1 << ": ";
+        cin >> nomes[i];
+    }
+
+    insertionSort(nomes, n);
+
+    cout << "\nNomes ordenados:" << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        cout << i+1 <<  "° " <<  nomes[i] << endl;
+    }
+
+    return 0;
+}
